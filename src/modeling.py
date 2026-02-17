f18002cf6c46a086f1619a0ea7239c86b0a3d482
Esther Toobian <etoobian@gmail.com>
2026-02-17 15:41:04 -0800

    src/modeling.py:  Update docstring; add it_rf_feature_screening().

--- src/modeling.py
+++ src/modeling.py
@@ -12,7 +12,9 @@ from __future__ import annotations
 
 from typing import List, Tuple
 
+import numpy as np
 import pandas as pd
+
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import OneHotEncoder
