5de40e8326e3da272dc597e0f953c45cbd6d5b1f
Esther Toobian <etoobian@gmail.com>
2026-02-21 04:06:14 -0800

    Updated src/schema.py moving alternative target vars to DROP_COLS.

--- src/schema.py
+++ src/schema.py
@@ -25,9 +25,8 @@ from typing import Dict, List, Tuple
 # -------- CORE COLUMNS --------
 TARGET_COL: str = "diagnosed_diabetes"
 
-# Data Leakage:
-# diabetes_stage is more granular version of our target
-DROP_COLS: List[str] = ["diabetes_stage",]
+# Remove Alternative Target Variables (avoid data leakage)
+DROP_COLS: List[str] = ["diabetes_stage", "diabetes_risk_score",]
 
 # -------- Feature groupings --------
 CATEGORICAL_COLS: List[str] = [
@@ -37,6 +36,7 @@ CATEGORICAL_COLS: List[str] = [
     "income_level",
     "employment_status",
     "smoking_status",
+    #"diabetes_stage",
 ]
 
 BINARY_COLS: List[str] = [
@@ -66,7 +66,7 @@ NUMERIC_COLS: List[str] = [
     "glucose_postprandial",
     "insulin_level",
     "hba1c",
-    "diabetes_risk_score",
+    #"diabetes_risk_score",
 ]
 
 
@@ -79,6 +79,7 @@ REF_ALLOWED_CATEGORIES: Dict[str, List[str]] = {
     "income_level": ["Low", "Medium", "High"],
     "employment_status": ["Employed", "Unemployed", "Retired", "Student"],
     "smoking_status": ["Never", "Former", "Current"],
+    #"diabetes_stage": ["No Diabetes", "Pre-Diabetes", "Type 1", "Type 2", "Gestational"],
 }
 
 REF_RANGE_RULES: Dict[str, Tuple[float, float]] = {
@@ -101,7 +102,7 @@ REF_RANGE_RULES: Dict[str, Tuple[float, float]] = {
     "glucose_postprandial": (90, 350),
     "insulin_level": (2, 50),
     "hba1c": (4, 14),
-    "diabetes_risk_score": (0, 100),
+    #"diabetes_risk_score": (0, 100),
 }
 
 
